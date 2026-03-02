4062fe4c464bacd2d1ba211c485706187e9d6a0c
AndersonLoop <coellojanderson@gmail.com>
2026-03-01 21:17:34 -0500

    Modificar pom.xml

---
+++ src/test/java/com/anderson/api/domain/medico/MedicoRepositoryTest.java
@@ -0,0 +1,14 @@
+/*    package com.anderson.api.domain.medico;
+
+    import org.junit.jupiter.api.Test;
+    import org.springframework.boot.test.autoconfigure.json.AutoConfigureJson;
+
+    @DataJpaTest
+    @
+    class MedicoRepositoryTest {
+
+        @Test
+        void elegirMedicoAleatorioDisponibleEnLaFecha() {
+
+        }
+    }*/
